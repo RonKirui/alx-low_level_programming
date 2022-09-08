16a720e4aa26353bd8e294801041a3804db6c404
Ronald Kiplangat <ronkiruiplandi@gmail.com>
2022-09-08 16:45:05 -0700

    Commit mandatory changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *
  *  * main - Entry point
@@ -10,6 +11,6 @@
  *     */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
