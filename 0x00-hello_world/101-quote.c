85e526518e24e682f1d73f5469523a1f6128db35
Ronald Kiplangat <ronkiruiplandi@gmail.com>
2022-09-08 17:29:05 -0700

    Commit mandatory changes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always return 1 on success
+ */
+
+int main(void)
+
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
