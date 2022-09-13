8f0088c12e2aa072242a7610ca6e9499472c9619
Ronald Kiplangat <ronkiruiplandi@gmail.com>
2022-09-12 19:16:43 -0700

    Commit message

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 /**
  * main - checks whether a random value is positive,
- * 	negative or zero and prints the outcome
- *
+ * negative or zero and prints the outcome
+ * 
  * Return:return value is always 0
  *
  */
@@ -12,7 +12,7 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
